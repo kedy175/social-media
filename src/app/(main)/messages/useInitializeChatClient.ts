339590bb7a1d763d2e122a63d870b907b9d14193
kedy175 <kyawyanlin.001@gmail.com>
2024-11-29 00:50:59 +0000

    fixed error msg type on useInitChatClient

--- src/app/(main)/messages/useInitializeChatClient.ts
+++ src/app/(main)/messages/useInitializeChatClient.ts
@@ -8,65 +8,33 @@ export default function useInitializeChatClient() {
   const [chatClient, setChatClient] = useState<StreamChat | null>(null);
 
   useEffect(() => {
-    if (!user?.id) {
-      console.error("User data is missing");
-      return;
-    }
+    const client = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY!);
 
-    if (!process.env.NEXT_PUBLIC_STREAM_KEY) {
-      console.error("Stream API key is missing");
-      return;
-    }
-
-    const client = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY);
-
-    const fetchToken = async (): Promise<string> => {
-      try {
-        const { token } = await kyInstance
-          .get("/api/get-token", { searchParams: { id: user.id } })
-          .json<{ token: string }>();
-        console.log("Token fetched successfully:", token); // Debugging
-        return token;
-      } catch (error) {
-        console.error("Error fetching token:", error);
-        throw new Error("Failed to fetch token");
-      }
-    };
-
-    const connectUser = async () => {
-      try {
-        const token = await fetchToken();
-        await client.connectUser(
-          {
-            id: user.id,
-            username: user.username,
-            name: user.displayName,
-            image: user.avatarUrl,
-          },
-          token
-        );
-        setChatClient(client);
-        console.log("User connected successfully:", user.id);
-      } catch (error) {
-        console.error("Error connecting user to StreamChat:", {
-          message: error.message,
-          code: error.code,
-          response: error.response?.data,
-        });
-      }
-    };
-
-    connectUser();
+    client
+      .connectUser(
+        {
+          id: user.id,
+          username: user.username,
+          name: user.displayName,
+          image: user.avatarUrl,
+        },
+        async () =>
+          kyInstance
+            .get("/api/get-token")
+            .json<{ token: string }>()
+            .then((data) => data.token),
+      )
+      .catch((error) => console.error("Failed to connect user", error))
+      .then(() => setChatClient(client));
 
     return () => {
-      console.log("Cleaning up StreamChat client...");
       setChatClient(null);
       client
         .disconnectUser()
-        .then(() => console.log("StreamChat user disconnected"))
-        .catch((error) => console.error("Failed to disconnect user:", error));
+        .catch((error) => console.error("Failed to disconnect user", error))
+        .then(() => console.log("Connection closed"));
     };
-  }, [user?.id, user?.username, user?.displayName, user?.avatarUrl]);
+  }, [user.id, user.username, user.displayName, user.avatarUrl]);
 
   return chatClient;
 }
